183105bb76b85e597ecb03cb8376a3b0ddaec2df
Indy Ng <writingindy@gmail.com>
2023-05-15 11:31:50 -0400

    Added some initial wrapper functions
    Adds functions that:
    - converts matrix A to NumPy array
    - converts matrix A to code
    - implements Ising matrix A (in NumPy array form)

---
+++ Student-Hub/Indy-Ng/vqls.py
@@ -0,0 +1,87 @@
+"""Helper function code"""
+
+from typing import (List, Tuple)
+
+import functools as ft
+
+import pennylane as qml
+from pennylane import numpy as np
+from pennylane import Identity, PauliX, PauliY, PauliZ
+
+
+pauli_dict = {"I": Identity.compute_matrix(), "X": PauliX.compute_matrix(), "Y": PauliY.compute_matrix(), "Z": PauliZ.compute_matrix()}
+
+def A_to_num (n_qubits: int, coefs: np.tensor, terms: List[str]):
+    
+    if len(coefs) != len(terms):
+        raise ValueError("Number of coefficients does not match number of terms.")
+    
+    if n_qubits <= 0:
+        raise ValueError("Number of qubits is not a number greater than 0.")
+    
+    terms_len = len(terms)
+    for i in range(terms_len):
+        if len(terms[i]) != n_qubits:
+            raise ValueError("Number of terms in each Pauli gate combination must be the same as number of qubits.")
+        
+
+    dim = 2**n_qubits
+    mat = np.zeros((dim, dim), dtype=np.complex64)
+
+    for (c, pauli) in zip(coefs, terms):
+        pauli = [pauli_dict[key] for key in pauli]
+        if pauli == ["I"]:
+            mat += c * ft.reduce(np.kron, pauli)
+        else:
+            mat += c * ft.reduce(np.kron, pauli)
+        
+    return mat
+
+
+# This function technically works BUT needs to be tested + optimized further
+
+def A_to_code (idx, ancilla_idx, terms: List[str]):
+        
+    if idx < 0:
+        raise ValueError("Index of linear combination must be >= 0.")
+    
+    target_pauli = list(terms[idx])
+    
+    order_idx = 0
+
+    for i in range(len(target_pauli)):
+        if target_pauli[i] == 'I':
+            order_idx += 1
+            None
+        if target_pauli[i] == 'X':
+            qml.CNOT(wires = (ancilla_idx, order_idx))
+            order_idx += 1
+        if target_pauli[i] == 'Y':
+            qml.CY(wires = (ancilla_idx, order_idx))
+            order_idx += 1
+        if target_pauli[i] == 'Z':
+            qml.CZ(wires = (ancilla_idx, order_idx))
+            order_idx += 1
+
+# This code works, but is adapted from Rigetti code
+
+def A_Ising_num (n: int, zeta: float, eta: float, J: float):
+    
+    if n <= 0:
+        raise ValueError("Number of qubits is not a number greater than 0.")
+    
+    Acoeffs = [eta/zeta]
+    Aterms = ["I" * n]
+
+    Acoeffs += [1/zeta] * n
+    xbase = "X" + "I" * (n - 1)
+    for ii in range(n, 0, -1):
+        Aterms.append(xbase[ii:] + xbase[:ii])
+
+    Acoeffs += [J/zeta] * (n - 1)
+    zzbase = "ZZ" + "I" * (n - 2)
+    for ii in range(n, 1, -1):
+        Aterms.append(zzbase[ii:] + zzbase[:ii])
+
+    return Acoeffs, Aterms
+      
