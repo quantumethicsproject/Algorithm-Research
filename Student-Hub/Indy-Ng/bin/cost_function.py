8ffdf0d5e44e6547ca67ed3d931e8583c462cacd
hametar0u <57603310+hametar0u@users.noreply.github.com>
2023-12-26 11:29:56 -0800

    refactored qnode to be defined outside of the cost function

--- Student-Hub/Indy-Ng/bin/cost_function.py
+++ Student-Hub/Indy-Ng/bin/cost_function.py
@@ -4,78 +4,73 @@ from bin.error_mitigation import mitigate_node
 
 import time
 
-def cost_loc(problem, weights, device):
+def local_hadamard_test(weights, problem, l=None, lp=None, j=None, part=None):
+
+    ancilla_idx = problem.get_n_qubits()
+
+    # First Hadamard gate applied to the ancillary qubit.
+    qml.Hadamard(wires=ancilla_idx)
+
+    # For estimating the imaginary part of the coefficient "mu", we must add a "-i"
+    # phase gate.
+    if part == "Im" or part == "im":
+        qml.PhaseShift(-np.pi / 2, wires=ancilla_idx)
+
+    # Variational circuit generating a guess for the solution vector |x>
+    problem.variational_block(weights)
+
+    # Controlled application of the unitary component A_l of the problem matrix A.
+    problem.CA(ancilla_idx, l)
+
+    # Adjoint of the unitary U_b associated to the problem vector |b>.
+    # In this specific example Adjoint(U_b) = U_b.
+    problem.U_b()
+
+    # Controlled Z operator at position j. If j = -1, apply the identity.
+    if j != -1:
+        qml.CZ(wires=[ancilla_idx, j])
+
+    # Unitary U_b associated to the problem vector |b>.
+    problem.U_b()
+
+    # Controlled application of Adjoint(A_lp).
+    # In this specific example Adjoint(A_lp) = A_lp.
+    problem.CA(ancilla_idx, lp)
+
+    # Second Hadamard gate applied to the ancillary qubit.
+    qml.Hadamard(wires=ancilla_idx)
+
+    # Expectation value of Z for the ancillary qubit.
+    return qml.expval(qml.PauliZ(wires=ancilla_idx))
+
+# Computes the mu coefficients
+def mu(weights, local_hadamard_test, problem, l=None, lp=None, j=None):
+    """Generates the coefficients to compute the "local" cost function C_L."""
+
+    # start = time.time()
+    mu_real = local_hadamard_test(weights, problem, l=l, lp=lp, j=j, part="Re")
+    mu_imag = local_hadamard_test(weights, problem, l=l, lp=lp, j=j, part="Im")
+    # print(f"mu: {time.time() - start:.2f}")
+
+    return mu_real + 1.0j * mu_imag
+
+def psi_norm(weights, c, local_hadamard_test, problem):
+    """Returns the normalization constant <psi|psi>, where |psi> = A |x>."""
+    norm = 0.0
+
+    for l in range(0, len(c)):
+        for lp in range(0, len(c)):
+            # start = time.time()
+            norm = norm + c[l] * np.conj(c[lp]) * mu(weights, local_hadamard_test, problem, l, lp, -1)
+            # print(f"norm accum ({l*len(c) + lp})")
+
+    return abs(norm)
+
+def cost_loc(problem, weights, local_hadamard_test):
     """Local version of the cost function. Tends to zero when A|x> is proportional to |b>."""
 
     c, _ = problem.get_coeffs()
     n_qubits = problem.get_n_qubits()
-    ancilla_idx = n_qubits
-
-    def local_hadamard_test(weights, l=None, lp=None, j=None, part=None):
-
-        # First Hadamard gate applied to the ancillary qubit.
-        qml.Hadamard(wires=ancilla_idx)
-
-        # For estimating the imaginary part of the coefficient "mu", we must add a "-i"
-        # phase gate.
-        if part == "Im" or part == "im":
-            qml.PhaseShift(-np.pi / 2, wires=ancilla_idx)
-
-        # Variational circuit generating a guess for the solution vector |x>
-        problem.variational_block(weights)
-
-        # Controlled application of the unitary component A_l of the problem matrix A.
-        problem.CA(ancilla_idx, l)
-
-        # Adjoint of the unitary U_b associated to the problem vector |b>.
-        # In this specific example Adjoint(U_b) = U_b.
-        problem.U_b()
-
-        # Controlled Z operator at position j. If j = -1, apply the identity.
-        if j != -1:
-            qml.CZ(wires=[ancilla_idx, j])
-
-        # Unitary U_b associated to the problem vector |b>.
-        problem.U_b()
-
-        # Controlled application of Adjoint(A_lp).
-        # In this specific example Adjoint(A_lp) = A_lp.
-        problem.CA(ancilla_idx, lp)
-
-        # Second Hadamard gate applied to the ancillary qubit.
-        qml.Hadamard(wires=ancilla_idx)
-
-        # Expectation value of Z for the ancillary qubit.
-        return qml.expval(qml.PauliZ(wires=ancilla_idx))
-    
-    local_hadamard_test = qml.QNode(local_hadamard_test, device, interface="autograd")
-
-    # local_hadamard_test = mitigate_node(local_hadamard_test)
-    
-    # Computes the mu coefficients
-    def mu(weights, l=None, lp=None, j=None):
-        """Generates the coefficients to compute the "local" cost function C_L."""
-
-        # start = time.time()
-        mu_real = local_hadamard_test(weights, l=l, lp=lp, j=j, part="Re")
-        mu_imag = local_hadamard_test(weights, l=l, lp=lp, j=j, part="Im")
-        # print(f"mu: {time.time() - start:.2f}")
-
-        return mu_real + 1.0j * mu_imag
-
-    def psi_norm(weights):
-        """Returns the normalization constant <psi|psi>, where |psi> = A |x>."""
-        norm = 0.0
-
-        for l in range(0, len(c)):
-            for lp in range(0, len(c)):
-                # start = time.time()
-                norm = norm + c[l] * np.conj(c[lp]) * mu(weights, l, lp, -1)
-                # print(f"norm accum ({l*len(c) + lp})")
-
-        return abs(norm)
-    
-    
     
     # start = time.time()
     mu_sum = 0.0
@@ -84,7 +79,7 @@ def cost_loc(problem, weights, device):
         for lp in range(0, len(c)):
             for j in range(0, n_qubits):
                 # start2 = time.time()
-                mu_sum = mu_sum + c[l] * np.conj(c[lp]) * mu(weights, l, lp, j)
+                mu_sum = mu_sum + c[l] * np.conj(c[lp]) * mu(weights, local_hadamard_test, problem, l, lp, j)
                 # print(f"mu sum accum ({l*len(c)*len(c) + lp*len(c) + j})")
 
     mu_sum = abs(mu_sum)
@@ -92,7 +87,7 @@ def cost_loc(problem, weights, device):
 
     # Cost function C_L
     # start = time.time()
-    res = 0.5 - 0.5 * mu_sum / (n_qubits * psi_norm(weights))
+    res = 0.5 - 0.5 * mu_sum / (n_qubits * psi_norm(weights, c, local_hadamard_test, problem))
     # print(f"normalize:\t{time.time() - start:.2f}")
     return res
 
